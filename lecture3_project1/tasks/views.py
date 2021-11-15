5e2189924ae621a73049a0953912e13b674c1118
ReX342 <occ.rex@gmail.com>
2021-11-15 19:56:20 +0100

    mange.py migrate

--- lecture3_project1/tasks/views.py
+++ lecture3_project1/tasks/views.py
@@ -10,8 +10,11 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
